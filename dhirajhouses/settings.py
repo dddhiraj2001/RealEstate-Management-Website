15bafd2aaf8f12ac2af447f545f8f6e8326b303d
dddhiraj2001 <dddhiraj2001@gmail.com>
2024-07-21 01:20:20 -0400

    Added the Admin Page and Styling to Admin Page along with Dynamic Content Display

--- dhirajhouses/settings.py
+++ dhirajhouses/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'dhirajhouses/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
